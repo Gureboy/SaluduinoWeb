9cc0dec884b9f8f963d44e3d044ad6a67aa1f7b9
Gureboy <43415644+Gureboy@users.noreply.github.com>
2024-08-15 15:40:21 -0300

    Update SaluduinoWeb.h

--- SaluduinoWeb.h
+++ SaluduinoWeb.h
@@ -2,29 +2,31 @@
 #define SALUDUINOWEB_H
 
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
+#include <FS.h>
 
 class SaluduinoWeb {
 public:
     SaluduinoWeb();
     void begin();
+    void handleClient();
     void handleRoot();
-    void handleRegister();
     void handleLogin();
-    void handleLogout();
-    void handleSuccess();
-    void handleError();
+    void handleRegister();
+    void handleVerPacientes();
     void handleOpcionesAvanzadas();
+    void handleHubEnfermero();
+    void handleAgregarPaciente();
+    void handleEliminarPaciente();
+    void handleNotFound();
     
 private:
     ESP8266WebServer server;
-    WiFiManager wifiManager;
-    String registeredUsername;
-    String registeredPassword;
-    String loggedInUser;
-    void sendResponse(String html);
+    
+    void loadFile(String path);
+    void handleFileRead(String path);
 };
 
 #endif
+
 
 
